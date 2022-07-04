8d2c7ebafa7e3565298c573dd35e2874627559ae
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-04 03:39:51 +0300

    Cards are done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import Card from './components/Card';
+import KatieZaferes from './assets/KatieZaferes.svg';
+import Wedding from './assets/wedding.svg';
+import Bike from './assets/bike.svg';
 
 const App = () => {
   return (
     <div className='App'>
         <Navbar />
         <HeroSection />
-        <Card />
+        <div className='App-cards'>
+            <Card img={KatieZaferes} rating='5.0' reviewCount='6' country='USA' title='Life lessons with Katie Zeferes' price='136' />
+            <Card img={Wedding} rating='5.0' reviewCount='30' country='USA' title='Learn wedding photography' price='125' />
+            <Card img={Bike} rating='4.8' reviewCount='2' country='USA' title='Group mountain biking' price='50' />
+        </div>
     </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import KatieZaferes from '../assets/KatieZaferes.svg';
 import Star from '../assets/star.svg';
 
-const Card = (props) => {
+const Card = ({img, rating, reviewCount, country, title, price}) => {
   return (
     <div className='card'>
         <div className='card-img'>
             <div className='card-img__photo'>
-                <img src={KatieZaferes} alt='woman' />
+                <img src={img} alt='woman' />
             </div>
         </div>
         <div className='card-text'>
             <div className='card-text__score'>
                 <img src={Star} alr='star' />
-                <span className='card-text__score-black'>5.0</span>
-                <span className='card-text__score-gray'>(6) USA</span>
+                <span className='card-text__score-black'>{rating}</span>
+                <span className='card-text__score-gray'>({reviewCount}) {country}</span>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
+            <p>{title}</p>
             <div className='card-text__price'>
-                <span>From $136</span>
+                <span>From ${price}</span>
                 <span>/ person</span>
             </div>
         </div>
